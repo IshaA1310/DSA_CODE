e6e1ec83c6f7054d257deaa683e245530b7bc11d
IshaA1310 <127334292+IshaA1310@users.noreply.github.com>
2025-09-09 12:26:41 +0530

    Update validParentheses.js

--- validParentheses.js
+++ validParentheses.js
@@ -1,24 +1,24 @@
 // check parenthessis valid or not 
-
-function validParentheses(str) {
-  let arr = [..str];
-  let stack = new Stack();
-  for (let i = 0; i < arr.length; i++) {
-    if(isOpen(arr[i])) {
-      stack.push(arr[i]);
-    } else {
-      if (stack.isEmpty()) return false;
-      const value = isValid(stack.pop(), arr[i]);
-      if(value === false) return false;
+var isValid = function (str) {
+    let arr = [...str];
+    let stack = new Stack();
+    for (let i = 0; i < arr.length; i++) {
+        if (isOpen(arr[i])) {
+            stack.push(arr[i]);
+        } else {
+            if (stack.isEmpty()) return false;
+            const s1 = stack.pop();
+            const value = validParentheses(s1, arr[i]);
+            if (value === false) return false;
+        }
     }
-  }
-  return true;
+    return stack.isEmpty();
 }
 
 function isOpen(value) {
-  return (value === '{' || value === '[' || value === '(');
+    return (value === '{' || value === '[' || value === '(');
 }
 
-function isValid(s1, s2) {
-  return ((s1 === '{' && s2 === '}') || (s1 === '[' && s2 === ']') || (s1 === '(' && s2 === ')'));
+function validParentheses(s1, s2) {
+    return ((s1 === '{' && s2 === '}') || (s1 === '[' && s2 === ']') || (s1 === '(' && s2 === ')'));
 }
